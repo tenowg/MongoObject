0b93ecb4678f228b3482912cd0e1cf796790a7fa
Craig Russell <tenowg@gmail.com>
2026-06-01 20:58:28 -0400

    Readded my test/example project, will change later

---
+++ Progress/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MongoObject.Core.Extensions;
+using Progress;
+using Progress.Extensions;
+
+AppDomain.CurrentDomain.ProcessExit += (sender, e) =>
+{
+    Console.WriteLine("Process is exiting!");
+    Console.WriteLine(Environment.StackTrace);
+};
+
+AppDomain.CurrentDomain.UnhandledException += (sender, e) =>
+{
+    Console.WriteLine($"Unhandled exception: {e.ExceptionObject}");
+};
+
+using IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((_, services) =>
+    {
+        services.AddMongoObject(options =>
+        {
+            options.ConnectionString = "mongodb://localhost:27017/?directConnection=true";
+            options.DatabaseName = "mydatabase";
+        })
+        .AddWatchStream()
+        .RegisterDocumentsProgress();
+        services.AddSingleton<App>();
+    })
+    .Build();
+
+// Run the app
+var app = host.Services.GetRequiredService<App>();
+await app.Run();
